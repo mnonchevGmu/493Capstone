e82b3c5dd3699fb4e648cf4c3e22e06b1e0e8e21
Monica Noncheva <61191527+mnonchevGmu@users.noreply.github.com>
2020-02-19 17:46:06 -0500

    Update welcome.blade.php
    Some other changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Something More
+                    Laravel 
                 </div>
 
                 <div class="links">

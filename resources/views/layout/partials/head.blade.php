61f27906fb6b02220f9ab7b9e22fa1feaf70fccd
Monica Noncheva <61191527+mnonchevGmu@users.noreply.github.com>
2020-03-18 19:57:35 -0400

    Partial blade html for reuse in search and results form

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,34 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Application built for Skydive Orange Customer Media registration">
+<meta name="author" content="FlyHigh">
+<title>Fly High</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/x.x.x/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <script type="text/javascript" src='scripts.js'>
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.4.0.min.js"
+        integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous">
+    </script>
+
+    <!--
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+-->
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/jquery.validate.min.js">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/additional-methods.js">
+    </script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"
+        integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+
+        <!-- Project specific style sheet -->
+        <link rel="stylesheet" type="text/css" href="{{ url('/css/projectStyles.css') }}" />
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,6 @@
+
+<div class="collapse bg-inverse" id="navbarHeader">
+     <div class="container">
+        <p>Nav goes here</p>
+    </div>
+</div>

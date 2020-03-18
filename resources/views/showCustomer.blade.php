d66abe83ed21bdd1fe2735aeba92518107d31db6
Monica Noncheva <61191527+mnonchevGmu@users.noreply.github.com>
2020-03-18 19:59:41 -0400

    showCustomer to display a single customer
    Need to resolve the 'CUSTOMER_ID' versus $customer call.  This is set up to receive ID currently.  This is just a shell

---
+++ resources/views/showCustomer.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+@include('layout.partials.head')
+
+
+</head>
+
+<body>
+
+@include('layout.partials.nav')
+
+<div class="container border border-dark p-3 bg-mediumgrey border-width-3">
+<p>Test</p>
+<br>
+<?php //echo $customer; ?>
+<?php  echo "<br>"; echo $id;?>
+</div>
+
+@include('layout.partials.footer-scripts')
+
+</body>
+</html>

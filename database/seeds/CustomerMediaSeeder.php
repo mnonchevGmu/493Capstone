ff892cc729c19d86f6f672aa59c10b9669c52806
Monica Noncheva <61191527+mnonchevGmu@users.noreply.github.com>
2020-03-18 19:55:54 -0400

    Added additional seeder records for Customer and Customer Media Tables

--- database/seeds/CustomerMediaSeeder.php
+++ database/seeds/CustomerMediaSeeder.php
@@ -27,5 +27,138 @@ class CustomerMediaSeeder extends Seeder
              'CREATION_DATE' => $currenttime,
              'LAST_UPDATE_DATE' => $currenttime
          ]);
+
+         DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 2,
+            'SKYDIVE_DATE' => '2019-03-11 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO+PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 3,
+            'SKYDIVE_DATE' => '2020-01-20 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 4,
+            'SKYDIVE_DATE' => '2020-03-12 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 0,
+            'USB_REQUIRED_ACK' => 1,
+            'MEDIA_TYPE' => 'VIDEO+PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 5,
+            'SKYDIVE_DATE' => '2020-02-24 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO+PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 6,
+            'SKYDIVE_DATE' => '2019-12-24 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 7,
+            'SKYDIVE_DATE' => '2019-11-19 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO+PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 8,
+            'SKYDIVE_DATE' => '2020-01-05 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 9,
+            'SKYDIVE_DATE' => '2020-01-09 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 10,
+            'SKYDIVE_DATE' => '2019-09-03 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO+PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 11,
+            'SKYDIVE_DATE' => '2019-10-09 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 12,
+            'SKYDIVE_DATE' => '2019-10-20 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        DB::table('CUSTOMER_MEDIA')->insert([
+            'CUSTOMER_ID' => 13,
+            'SKYDIVE_DATE' => '2019-07-20 10:00:00',
+            'DELIVERY_TIME_ACK' => 1,
+            'MEDIA_TERMS_ACK' => 1,
+            'USB_REQUIRED_ACK' => 0,
+            'MEDIA_TYPE' => 'VIDEO+PHOTOS',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
     }
-}
+    }
+
--- database/seeds/CustomersTableSeeder.php
+++ database/seeds/CustomersTableSeeder.php
@@ -25,5 +25,160 @@ class CustomersTableSeeder extends Seeder
             'CREATION_DATE' => $currenttime,
             'LAST_UPDATE_DATE' => $currenttime
         ]);
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Mario',
+            'LAST_NAME' => 'Ripa',
+            'EMAIL_ADDRESS' => 'mripa@yahoo.com',
+            'PHONE_NUMBER' => '540-672-5054',
+            'PHONE_TYPE' => 'WORK',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Pam',
+            'LAST_NAME' => 'Young',
+            'EMAIL_ADDRESS' => 'pyoung@hotmail.com',
+            'PHONE_NUMBER' => '202-437-6578',
+            'PHONE_TYPE' => 'HOME',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Whitney',
+            'LAST_NAME' => 'Payne',
+            'EMAIL_ADDRESS' => 'wpayne@gmail.com',
+            'PHONE_NUMBER' => '703-456-0494',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Ben',
+            'LAST_NAME' => 'Shank',
+            'EMAIL_ADDRESS' => 'bshank@skydiveorange.com',
+            'PHONE_NUMBER' => '571-908-3940',
+            'PHONE_TYPE' => 'HOME',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Lauren',
+            'LAST_NAME' => 'Conway',
+            'EMAIL_ADDRESS' => 'lconway@yahoo.com',
+            'PHONE_NUMBER' => '703-495-3748',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Brad',
+            'LAST_NAME' => 'Cohen',
+            'EMAIL_ADDRESS' => 'bcohen@gmail.com',
+            'PHONE_NUMBER' => '571-890-2749',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Chris',
+            'LAST_NAME' => 'Whitley',
+            'EMAIL_ADDRESS' => 'cwhitley@verizon.net',
+            'PHONE_NUMBER' => '540-876-2744',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Kat',
+            'LAST_NAME' => 'Osipova',
+            'EMAIL_ADDRESS' => 'kosipova@msn.com',
+            'PHONE_NUMBER' => '202-345-8593',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Jon',
+            'LAST_NAME' => 'Stotts',
+            'EMAIL_ADDRESS' => 'jstotts@aol.com',
+            'PHONE_NUMBER' => '540-384-3380',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Rose',
+            'LAST_NAME' => 'Smith',
+            'EMAIL_ADDRESS' => 'rsmith@usmc.com',
+            'PHONE_NUMBER' => '703-289-4749',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'Kevin',
+            'LAST_NAME' => 'Haugh',
+            'EMAIL_ADDRESS' => 'khaugh@usmc.com',
+            'PHONE_NUMBER' => '571-859-3849',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
+
+        // Date and time of creation
+        $currenttime =Carbon::now();
+        // Insert VIDEO type       
+        DB::table('CUSTOMERS')->insert([
+            'FIRST_NAME' => 'John',
+            'LAST_NAME' => 'Lighthall',
+            'EMAIL_ADDRESS' => 'jlighthall@gmail.com',
+            'PHONE_NUMBER' => '540-374-3948',
+            'PHONE_TYPE' => 'CELL',
+            'CREATION_DATE' => $currenttime,
+            'LAST_UPDATE_DATE' => $currenttime
+        ]);
     }
 }

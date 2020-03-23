fd190cd2bdcc158fc80eb9728594a9bc674e9b70
Monica Noncheva <61191527+mnonchevGmu@users.noreply.github.com>
2020-03-23 13:45:52 -0400

    Fixed problem with Query Controller and NULLs

--- app/Http/Controllers/customerQueryController.php
+++ app/Http/Controllers/customerQueryController.php
@@ -21,15 +21,44 @@ class customerQueryController extends Controller
         $customerQuery = $customer->newQuery();
         // First Name
 
-        if ($request->has('FirstName')) {
-            $customerQuery->where('FIRST_NAME', $request->input('FirstName'));
+        if (($request->has('customerFirstName')) AND ($request->input('customerFirstName'))!== null) {
+            $customerQuery->where('FIRST_NAME', $request->input('customerFirstName'));
         }
 
         // Last Name
-
+        if (($request->has('customerLastName')) AND ($request->input('customerLastName'))!== null ) {
+            $customerQuery->where('LAST_NAME', $request->input('customerLastName'));
+        }
+        // Email
+        if (($request->has('customerEmail1')) AND ($request->input('customerEmail1'))!== null ) {
+            $customerQuery->where('EMAIL_ADDRESS', $request->input('customerEmail1'));
+        }
+        
+        // Phone Number
+        if (($request->has('customerPhoneNum')) AND ($request->input('customerPhoneNum'))!== null )  {
+            $customerQuery->where('PHONE_NUMBER', $request->input('customerPhoneNum'));
+        }
+        
+        // Phone Type
+        if (($request->has('customerPhoneType')) AND ($request->input('customerPhoneType'))!== null )  {
+            $customerQuery->where('PHONE_TYPE', $request->input('customerPhoneType'));
+        }
+        
+        /*start of Monica changes
 
         // Media Type
-
+        //
+        
+        if ($request->has('videoOnly')) {
+            $customerQuery->where('VIDEO', $request->input('videoOnly'));
+        }
+        else if ($request->has('photoOnly')){
+            $customerQuery->where('PHOTOS', $request->input('photoOnly'));
+        }
+        else ($request->has('VideoAndPhotos')){
+            $customerQuery->where('VIDEO+PHOTOS', $request->input('VideoAndPhotos'));
+        }
+        END*/
         // Get the results and return them.
         //can i just return the view with the data? like ...
 
--- resources/views/customerQueryForm.blade.php
+++ resources/views/customerQueryForm.blade.php
@@ -18,25 +18,48 @@
 <form action="{{ action('customerQueryController@filter')}} " method="POST">
 
   <div class="form-group">
-    <label for="FirstName">First Name</label>
-    <input type="text" class="form-control" id="FirstName"  placeholder="First Name" name="FirstName">
+    <label for="customerFirstName">First Name</label>
+    <input type="text" class="form-control" id="customerFirstName"  placeholder="First Name" name="customerFirstName">
    </div>
    <div class="form-group">
-    <label for="LastName">Last Name</label>
-    <input type="text" class="form-control" id="LastName"  placeholder="Last Name" name="LastName">
+    <label for="customerLastName">Last Name</label>
+    <input type="text" class="form-control" id="customerLastName"  placeholder="Last Name" name="customerLastName">
    </div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="VideoOnly" value="VIDEO">
-  <label class="form-check-label" for="VideoOnly">Video</label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="PhotoOnly" value="PHOTOS">
-  <label class="form-check-label" for="PhotoOnly">Photos</label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="VideoAndPhotos" value="VIDEO+PHOTOS" >
-  <label class="form-check-label" for="VideoAndPhotos">Video and Photos</label>
-</div>
+
+<div class="form-group">
+    <label for="customerEmail1">Customer Email</label>
+    <input type="text" class="form-control" id="customerEmail1"  placeholder="Customer Email" name="customerEmail1">
+   </div>
+
+    <!-- TODO put the phone and type on the same row -->
+   <div class="form-group">
+    <label for="customerPhoneNum">Customer Phone Number</label>
+    <input type="text" class="form-control" id="customerPhoneNum"  placeholder="Customer Phone Number" name="customerPhoneNum">
+   </div>
+   
+   <!-- TODO make the phone type pull from the database and make a dropdown -->
+   <div class="form-group">
+    <label for="customerPhoneType">Customer Phone Type</label>
+    <input type="text" class="form-control" id="customerPhoneType"  placeholder="Customer Phone Number" name="customerPhoneType">
+   </div>
+
+<fieldset class="mediaType-border border border-dark rounded p-2">
+    <legend class = "mediaType-border">Media Type</legend>
+
+        <div class="form-check form-check-inline">
+        <input class="form-check-input m-2" type="radio" name="customerMediaSelection" id="customerMediaSelection" value="VIDEO">
+        <label class="form-check-label m-2" for="VideoOnly">Video</label>
+        </div>
+        <div class="form-check form-check-inline">
+        <input class="form-check-input m-2" type="radio" name="customerMediaSelection" id="customerMediaSelection" value="PHOTOS">
+        <label class="form-check-label m-2" for="PhotosOnly">Photos</label>
+        </div>
+        <div class="form-check form-check-inline">
+        <input class="form-check-input m-2" type="radio" name="customerMediaSelection" id="customerMediaSelection" value="VIDEO+PHOTOS" >
+        <label class="form-check-label m-2" for="VideoAndPhotos">Video and Photos</label>
+        </div>
+</fieldset>
+
 <br>
      @csrf
   <button type="submit" class="btn btn-primary" formmethod="post">Submit</button>

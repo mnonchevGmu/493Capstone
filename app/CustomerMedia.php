d27dc500ae8d640348a8bbcdb934bf739a44db65
Monica Noncheva <61191527+mnonchevGmu@users.noreply.github.com>
2020-03-23 13:47:50 -0400

    Create CustomerMedia.php

---
+++ app/CustomerMedia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerMedia extends Model
+{
+    //Define the table 
+    protected $table = 'CUSTOMER_MEDIA';
+    protected $primaryKey = 'CUSTOMER_MEDIA_ID';
+    //TODO: validate date format
+    //U is timestamp (seconds from epoch)
+    //Y-m-d H:i:s is the Carbon format default (?)
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    //protected $connection = 'mysql';
+
+}
